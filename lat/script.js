8cc0ecb65f6b6124658464b129155c633f6eaa7f
seasonalexplore <seasonalexploremaldives@gmail.com>
2026-02-12 04:05:16 -0500

    Delete lat directory

--- lat/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const iframe = document.querySelector("iframe");
-    iframe.onload = () => {
-        console.log("Tally form loaded successfully.");
-    };
-});
